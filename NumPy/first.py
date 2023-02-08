6d1dcda89caaaea032aa75d1f7b622733eac4ca7
Yashas M B <89311367+yashasmb@users.noreply.github.com>
2023-02-08 23:39:46 +0530

    first

---
+++ NumPy/first.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+print(a)
+print(a.shape)
+print(a.dtype)
+print((a.ndim))
+
+# a = np.arange(6)
+# a2 = a[np.newaxis, :]
+# print(a2.shape)
+
+
+# a = np.array([1, 2, 3, 4, 5, 6])
+
+# # print(type(a.len))
+
+# a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+# print(a)
